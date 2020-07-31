1b289b1af9dc1021ff333667f9a68f11574dfa74
Luiz Mello <adao.luiz.mello@gmail.com>
2020-07-30 21:38:09 -0300

    Segunda Aula Imersão React

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Logo_SWT from '../../assets/img/Logo_SWT.png';
+import './Footer.css';
 import { FooterBase } from './styles';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="http://www.swtag.com.br/">
+      <img className="Logo_SWT" src={Logo_SWT} alt="SWT Logo"/>
       </a>
       <p>
         Orgulhosamente criado durante a
